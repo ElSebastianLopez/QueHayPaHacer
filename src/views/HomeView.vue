cd6dd865cb54ccd16f263d1666904692edcce998
ElSebastianLopez <sebaslopezhenao@gmail.com>
2022-09-24 22:38:18 -0500

    add boostrap, add carusel and cards at home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  <div>
+     <div>
+       <CarruselCom />
+     </div>
+     <div class="dd">
+       <div class="card1">
+         <card-component />
+         <card-component />
+         <card-component />
+         <card-component />
+       </div>
+     </div>
+   </div>
+ </template>
+ 
+ <script>
+ // @ is an alias to /src
+ import CarruselCom from "@/components/CarruselCom.vue";
+ import CardComponent from "@/components/CardComponent.vue";
+ 
+ export default {
+   components: {
+     CarruselCom,
+     CardComponent
+   },
+ };
+ </script>
+ 
+ <style scoped>
+ .card1 {
+   width: 100%;
+ 
+   margin-top: 20px;
+   display: grid;
+   grid-template-columns: repeat(3, 1fr);
+   grid-template-rows: repeat(3, 1fr);
+ }
+ .dd {
+   margin: auto;
+ }
+ </style>
+ 
