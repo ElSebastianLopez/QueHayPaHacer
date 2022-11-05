9a0f67fb19059cf0fb83aa7fe4fef5257a0af003
ElSebastianLopez <sebaslopezhenao@gmail.com>
2022-11-05 18:42:48 -0500

    Implmento el backend del proyecto

---
+++ src/views/Admin/VistaAddOrEdit.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="row g-3 ancho">
+        <div class="col-md-6">
+            <label for="prueba" class="form-label">Nombre Lugar</label>
+            <input v-model="nombreLugarf" class="form-control" type="text" required>
+        </div>
+        <div class="col-md-6">
+            <label for="" class="form-label">Direccion Lugar</label>
+            <input v-model="direccionLugarf" type="text" class="form-control" required>
+        </div>
+        <div class="col-md-6">
+            <label for="" class="form-label">Descripcion Lugar</label>
+            <input v-model="descripcionLugarf" type="text" class="form-control" required>
+        </div>
+        <div class="col-md-6">
+            <label for="" class="form-label">Fecha Publicacion</label>
+            <input v-model="fechaPublicacionf" type="text" class="form-control" required>
+        </div>
+        <div class="col-md-6">
+            <label for="" class="form-label">Url Imagen</label>
+            <input v-model="urlImagenf" type="url" class="form-control"  required>
+        </div>
+        <div class="col-md-6">
+            <label for="" class="form-label">Descripcion Publicacion</label>
+            <input v-model="descripcionPublicacionf" type="text" class="form-control" required>
+        </div>
+
+        <button @click="CrearApiPublicacion()" type="button" class="btn btn-primary">Guardar</button>
+
+        <a href="/Admin"><button type="button" class="btn btn-primary">Regresar Al panel principal</button></a>
+
+        
+    </div>
+   
+</template>
+
+<script>
+
+import axios from "axios";
+
+import Swal from 'sweetalert2';
+
+export default {
+    data(){
+        return{
+          
+            
+        }
+
+    },
+    
+    methods:{
+        CrearApiPublicacion(){
+            var crearPublicacion=
+            {
+                direccionLugar: this.direccionLugarf,
+                nombreLugar: this.nombreLugarf,
+                descripcionLugar: this.descripcionLugarf,
+                fechaPublicacion: "2022-10-27T05:25:34.010Z",
+                urlImagen: this.urlImagenf,
+                descripcionPublicacion: this.descripcionPublicacionf
+            }
+            
+      axios.post("https://localhost:7157/api/PublicacionLugares",crearPublicacion).then((res)=>{
+          //crearPublicacion(res.data)
+          if(res.data.result== "Registro Creado Correctamente "){
+            this.mensaje('Registro Creado Correctamente');
+          }
+          else{
+            this.mensaje('No se a podido crear el registro  ');
+          }
+      })
+      .catch((e)=>{
+        console.log(e)
+      })
+
+    },
+
+    mensaje(mensaje){
+      Swal.fire(mensaje)
+    }, 
+    }
+
+    
+}
+</script>
+
+<style>
+
+.ancho{
+    width: 80%;
+    margin: auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,12 @@
       <CarruselCom />
     </div>
     <div class="card1">
-      <card-component />
-      <card-component />
-      <card-component />
-      <card-component />
+      <card-component v-for="lugares in resultadoPubli" v-bind:key="lugares.idLugar"  
+       :tituloPrueba="lugares.nombreLugar"
+       :descripcion="lugares.descripcionLugar"
+       :urlImagen="lugares.urlImagen"
+       />
+    
     </div>
   </div>
 </template>
@@ -16,12 +18,32 @@
 // @ is an alias to /src
 import CarruselCom from "@/components/CarruselCom.vue";
 import CardComponent from "@/components/CardComponent.vue";
+import axios from "axios";
 
 export default {
   components: {
     CarruselCom,
     CardComponent,
   },
+  data(){
+    return{
+      resultado:[],
+      resultadoPubli:{},
+    }
+   },
+   methods: {
+    consultarApiPublicacion(){
+      axios.get("https://localhost:7157/api/PublicacionLugares").then((res)=>{
+         console.log("publicaciones",res.data);
+         this.resultadoPubli=res.data;
+      })
+
+    },
+  },
+  mounted(){
+    this.consultarApiPublicacion();
+    
+    }
 };
 </script>
 
