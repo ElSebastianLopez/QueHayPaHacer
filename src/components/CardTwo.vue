d7596d95f06a609c2268d85137c175c63c307e21
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-11-07 02:17:44 -0500

    grid auto-fill

--- src/components/CardTwo.vue
+++ src/components/CardTwo.vue
@@ -1,24 +1,128 @@
 <template>
-  <div>
-    <div class="card" style="width: 23rem; height: 450px;margin-top: 20px;border-radius: 20px;">
-      <img :src="urlImagen" class="card-img-top"  alt="Card image cap" style="width:330px;height:180px; margin: auto;border-radius: 20px;padding-top: 10px;">
-      <div class="card-body">
-        <h5 class="card-title">{{tituloPrueba}}</h5>
-        <p class="card-text">{{descripcion}}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div
+    class="card"
+    style="width: 20rem; height: 380px; margin-top: 20px; border-radius: 20px"
+  >
+    <img
+      :src="urlImagen"
+      class="card-img-top"
+      alt="Card image cap"
+      style="
+        width: 300px;
+        height: 180px;
+        margin: auto;
+        border-radius: 20px;
+        padding-top: 10px;
+      "
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ tituloPrueba }}</h5>
+      <p class="card-text des">{{ descripcion }}</p>
+      <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+      <button
+        type="button"
+        class="btn btn-primary mt-2"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+        data-bs-whatever="@mdo"
+      >
+        Leer más...
+      </button>
+    </div>
+  </div>
+
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">
+            Detalle: {{ tituloPrueba }}
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="aline pr">
+            <!--<div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>-->
+            <small class="text-muted pr">{{ fechaPubli }}</small>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <img
+                :src="urlImagen"
+                class="card-img-top"
+                alt="Card image cap"
+                style="
+                  width: 400px;
+                  height: 250px;
+                  margin: auto;
+                  border-radius: 20px;
+                  padding-top: 10px;
+                "
+              />
+              <div class="card-body">
+                <h5 class="card-title">{{ tituloPrueba }}</h5>
+                <p class="card-text des">{{ descripcion }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-  
-    name: 'CardComponent',
-    props: {
-      tituloPrueba:String,
-      descripcion:String,
-      urlImagen:Image,
-    },
-  }
-  </script>
+export default {
+  name: "CardComponent",
+  props: {
+    tituloPrueba: String,
+    descripcion: String,
+    urlImagen: Image,
+    fechaPubli: Date,
+  },
+};
+</script>
+<style>
+.des {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 6;
+  line-clamp: 6;
+  overflow: hidden;
+  cursor: pointer;
+}
+.des:hover {
+  display: block;
+}
+.pr {
+  max-width: 82px;
+  white-space: nowrap;
+  overflow: hidden;
+}
+.aline{
+  text-align: right;
+}
+</style>
